61b6537ec3a2c88c00481bd54d469ac1878c7db0
Jann_Z <81670592+Jann-Z@users.noreply.github.com>
2026-09-09 11:09:47 +0900

    Create 12.dtrios.combine.sh

---
+++ .github/workflows/demographic_history/12.dtrios.combine.sh
@@ -0,0 +1,4 @@
+#combine Dtrios of the different chromosomes into one file
+# tree.nwk is created in APE, see script 11.reduce.tree.R
+/apps/Dsuite/Build/Dsuite DtriosCombine -o trios_combined -t population_tree_for_dsuite.nwk \
+chr1.trio chr2.trio chr3.trio chr4.trio chr5.trio chr6.trio chr7.trio chr8.trio chr9.trio chr10.trio chr11.trio chr12.trio chr13.trio chr14.trio chr15.trio chr16.trio chr17.trio chr18.trio chr19.trio chr20.trio chr21.trio chr22.trio chr23.trio chr24.trio
